d09435a0469ded99296616981f33ef8759380806
Aze <syedazeez337@gmail.com>
2024-08-06 08:31:34 -0400

    first programme

---
+++ Chapter_01/first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int num;
+
+    printf("I am a simple ");
+    printf("computer.\n");
+    printf("Enter your favourite computer: ");
+    scanf("%d", &num);
+    printf("My favourite number is %d...\n", num);
+
+    return 0;
+}
